e71064f806af28584096d0aa1b1ac66b4e5d7f18
Mohammad Samin-Al-Wasee <sameenwasee@gmail.com>
2025-01-26 10:37:14 +0600

    [config] Update dependencies
    - Add :credo and :dotenvy

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule Felixir.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:dotenvy, "~> 0.8.0"},
+      {:credo, "~> 1.5", only: [:dev, :test]}
     ]
   end
 
